d50a0f041a8522b5be47aadd607e5b854b5d104a
Marc Stroebel <mdstroebel@gmail.com>
2022-07-04 09:59:34 +1000

    feat: added Pointzi packages

--- MessagingAppChallenge/MessagingAppChallengeApp.swift
+++ MessagingAppChallenge/MessagingAppChallengeApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import StreetHawkCore_Pointzi
 
 @main
 struct MessagingAppChallengeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             MessageListView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        Pointzi.sharedInstance().registerInstall(forApp: "MessagingApp", withDebugMode: true) {
+            Pointzi.sharedInstance().tagCuid("mdstroebel@gmail.com")
+        }
+        return true
     }
 }
