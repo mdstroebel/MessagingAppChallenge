59268ef1454e50233585618bfe7a71287a74b1f1
Marc Stroebel <mdstroebel@gmail.com>
2022-07-03 13:27:16 +1000

    Initial Commit

---
+++ MessagingAppChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MessagingAppChallenge
+//
+//  Created by 24com on 3/7/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MessagingAppChallenge/MessagingAppChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagingAppChallengeApp.swift
+//  MessagingAppChallenge
+//
+//  Created by 24com on 3/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct MessagingAppChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
